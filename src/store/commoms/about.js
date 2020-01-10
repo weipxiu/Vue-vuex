1e88791ced2fa13bdf923daf25da904e1becce0a
李俊 <lijun@021.com>
2020-01-10 20:45:58 +0800

    增加错误提示

--- src/store/commoms/about.js
+++ src/store/commoms/about.js
@@ -1,16 +1,16 @@
 export default {
   namespaced: true,
   state: {
-    msg:'这是关于我页面！'
+    msg: '这是关于我页面！'
   },
   mutations: {
     save: (state, payload) => {
-      console.log('payload',payload)
+      console.log('payload', payload)
       Object.keys(payload).forEach(e => {
         if (this.hasOwnProperty(e)) {
           state[e] = payload[e]
-        }else{
-          console.error(e+'当前属性不存在')
+        } else {
+          console.error('Sorry，更新失败：属性' + e + '在对应的modules模块中未定义！')
         }
       })
     }
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,8 +1,12 @@
 export default {//payload接受页面事件交互传过来的参数值
   save: (state, payload) => {
-    console.log('payload',payload)
+    console.log('payload', payload)
     Object.keys(payload).forEach(e => {
-      state[e] = payload[e]
+      if (this.hasOwnProperty(e)) {
+        state[e] = payload[e]
+      } else {
+        console.error('Sorry，更新失败：属性' + e + '在对应的modules模块中未定义！')
+      }
     })
   },
   addIncrement(state, payload) {
